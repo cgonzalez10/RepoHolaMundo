e1a9f97cd08e4d0bbafb0630317f4045abaf0567
Cristina Gonzalez <cristina.gonzalez.cordova@tec.mx>
2025-02-26 09:19:21 -0600

    Cambios en Privacy

--- WebApplication1/Pages/Privacy.cshtml.cs
+++ WebApplication1/Pages/Privacy.cshtml.cs
@@ -5,12 +5,6 @@ namespace WebApplication1.Pages;
 
 public class PrivacyModel : PageModel
 {
-    private readonly ILogger<PrivacyModel> _logger;
-
-    public PrivacyModel(ILogger<PrivacyModel> logger)
-    {
-        _logger = logger;
-    }
 
     public void OnGet()
     {
